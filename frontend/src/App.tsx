ac20cec304fc5595645c2030c79297a5bbcd2088
testA <iamtester4321@gmail.com>
2025-05-02 17:33:27 +0530

    bit added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/header";
+import Input from "./components/input";
 
 export function App() {
   return (
     <html>
       <body>
         <Header />
+        <Input/>
       </body>
     </html>
   );
