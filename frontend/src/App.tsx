c746b36431ca7a07d19c068fed251f0c6880b5b2
tester <iamtester4321@gmail.com>
2025-05-06 18:40:01 +0530

    on submit changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,18 @@
-import Header from "./components/header"
-import Input from "./components/input"
-import ReactDOM from "react-dom/client";
+import Input from "./components/input";
 import LogIn from "./log-in/logIn";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
-
   return (
     <>
-
-<BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Input />} />
-        <Route path="/login" element={< LogIn/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Input />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
