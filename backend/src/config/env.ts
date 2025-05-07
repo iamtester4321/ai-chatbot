960019f90f02cdda20cf8fbe34ac8b9fe2e9d438
test <iamtester4321@gmail.com>
2025-05-07 13:47:25 +0530

    auth completed oauth 20% remains

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,8 +6,9 @@ dotenv.config();
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   //REDIS_URL: z.string().url(),
-  //GOOGLE_CLIENT_ID: z.string(),
-  //GOOGLE_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  GOOGLE_CALLBACK_URL: z.string().url(),
   GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
   JWT_SECRET: z.string(),
   SESSION_SECRET: z.string(),
