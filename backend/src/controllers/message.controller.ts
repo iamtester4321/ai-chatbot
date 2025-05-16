0f231e976c421ad5ef849b47f2b6be8da725c4fb
tester <iamtester4321@gmail.com>
2025-05-16 18:21:59 +0530

    yogesh/perament fix to any problem

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,38 +1,44 @@
 import { Request, Response } from "express";
-import { updateMessageReactionLikeService, updateMessageReactionDislikeService } from "../services/message.service";
+import {
+  updateMessageReactionLikeService,
+  updateMessageReactionDislikeService,
+} from "../services/message.service";
+import asyncHandler from "express-async-handler";
 
-export const updateMessageReactionLike = async (req: any, res: any) => {
-  const { messageId } = req.params;
-  const { liked } = req.body;
+export const updateMessageReactionLike = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { messageId } = req.params;
+    const { liked } = req.body;
 
-  try {
-    const updatedMessage = await updateMessageReactionLikeService(
-      messageId,
-    );
-    return res.status(200).json({
-      message: "Message reaction updated successfully",
-      data: updatedMessage,
-    });
-  } catch (error: any) {
-    console.error("Error updating message reaction:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    try {
+      const updatedMessage = await updateMessageReactionLikeService(messageId);
+      res.status(200).json({
+        message: "Message reaction updated successfully",
+        data: updatedMessage,
+      });
+    } catch (error: any) {
+      console.error("Error updating message reaction:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
-};
+);
 
-export const updateMessageReactionDislike = async (req: any, res: any) => {
-  const { messageId } = req.params;
-  const { disliked } = req.body;
+export const updateMessageReactionDislike = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { messageId } = req.params;
+    const { disliked } = req.body;
 
-  try {
-    const updatedMessage = await updateMessageReactionDislikeService(
-      messageId,
-    );
-    return res.status(200).json({
-      message: "Message reaction updated successfully",
-      data: updatedMessage,
-    });
-  } catch (error: any) {
-    console.error("Error updating message reaction:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    try {
+      const updatedMessage = await updateMessageReactionDislikeService(
+        messageId
+      );
+      res.status(200).json({
+        message: "Message reaction updated successfully",
+        data: updatedMessage,
+      });
+    } catch (error: any) {
+      console.error("Error updating message reaction:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
-};
+);
