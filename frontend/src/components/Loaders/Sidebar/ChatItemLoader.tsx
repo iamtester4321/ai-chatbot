9a7efcf69d79f7aa71795d1f845fcbb2a5a4263c
RaseshLimbad <iamtester4321@gmail.com>
2025-05-20 18:26:58 +0530

    Rasesh/Sidebar Skeleton fix

--- frontend/src/components/Loaders/Sidebar/ChatItemLoader.tsx
+++ frontend/src/components/Loaders/Sidebar/ChatItemLoader.tsx
@@ -2,7 +2,7 @@ import Skeleton from "../Skeleton";
 
 const ChatItemLoader = () => {
   return (
-    <div className="flex items-center justify-between p-2.5 text-sm rounded-lg mb-1.5 w-full hover:bg-[var(--color-hover-bg)]">
+    <div className="flex items-center justify-between p-2.5 text-sm rounded-lg mb-1.5 w-full">
       <Skeleton className="h-5 w-[70%]" />
       <div className="flex items-center gap-2">
         <Skeleton className="h-5 w-5 rounded-full" />
